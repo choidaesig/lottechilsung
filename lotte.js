c86af0d28e1c36983d3c32f5cd89d5e56103613a
choidaesig <72694569+choidaesig@users.noreply.github.com>
2022-01-19 19:26:38 +0900

    Add files via upload

--- lotte.js
+++ lotte.js
@@ -1,18 +1,15 @@
-document.getElementsByClassName('swiper-button-next').addEventListener('click', function() {
-  intervalID = setInterval(show,200);
-});
-
-function show(){
-  var div = document.querySelector(".img");
-  opacity = Number(window.getComputedStyle(div).getPropertyValue("opacity"));
-  
-  if(opacity<1){
-        //Fade in 핵심 부분
-    opacity = opacity+0.1;
-    div.style.opacity=opacity;
-    //img.style.opacity=opacity;
+	//mainCon1 Motion
+  if($(window).scrollTop() > $(".maintext").offset().top - 500){
+    TweenMax.to($(".maintext .bigtext"), 1, {top:0, opacity:1, ease:Power3.easeOut});
+    TweenMax.to($(".maintext .smalltext"), 1, {top:0, opacity:1, delay:0.3, ease:Power3.easeOut});
   }
-  else{
-    clearInterval(intervalID);
+  if($(window).scrollTop() > $(".maintext").offset().top + 300){
+    TweenMax.to($(".maintext"), 1, {opacity:0, ease:Power3.easeOut});
+  }else{
+    TweenMax.to($(".maintext"), 1, {opacity:1, ease:Power3.easeOut});
   }
-}	
+  if($(window).scrollTop() > $(".maintext").offset().top){
+    $(".maintext").addClass("on");
+  }else{
+    $(".maintext").removeClass("on");
+  }
